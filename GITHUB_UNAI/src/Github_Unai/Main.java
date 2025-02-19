07628892579b4a662031e6cb4b15a8ca32c1d641
dam1 <dam1@A80533400716691>
2025-02-19 13:32:31 +0100

    WEW

--- GITHUB_UNAI/src/Github_Unai/Main.java
+++ GITHUB_UNAI/src/Github_Unai/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 	System.out.println("Hola mon!");
 	System.out.println("s0ha modificat git");
+	System.out.println("1a modificació Git"); 
 	}
 }
