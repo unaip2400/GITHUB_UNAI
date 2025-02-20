9979eae6e9e3a76599eae49c27ccecf06b73d778
unaip2400 <upalaciosmedina@gmail.com>
2025-02-20 10:13:44 +0100

    2a modificació

--- GITHUB_UNAI/src/Github_Unai/Main.java
+++ GITHUB_UNAI/src/Github_Unai/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 	System.out.println("Hola mon!");
 	System.out.println("s0ha modificat git");
-	System.out.println("1a modificació Git"); 
+	System.out.println("1a modificació Git");
+	System.out.println("2a modificació GitHub");
 	}
 }
