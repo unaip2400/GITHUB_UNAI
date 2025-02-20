a718a55476dfbd2ca01661b6f934315e51252802
dam1 <dam1@A80533400716691>
2025-02-20 10:22:41 +0100

    2a modificació

--- GITHUB_UNAI/src/Github_Unai/Main.java
+++ GITHUB_UNAI/src/Github_Unai/Main.java
@@ -2,8 +2,9 @@ package Github_Unai;
 
 public class Main {
 	public static void main(String[] args) {
-	System.out.println("Hola mon!");
-	System.out.println("s0ha modificat git");
-	System.out.println("1a modificació Git"); 
+		System.out.println("Hola mon!");
+		System.out.println("s0ha modificat git");
+		System.out.println("1a modificació Git");
+	
 	}
 }
